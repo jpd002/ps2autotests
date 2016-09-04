bbb4c4ed5d46baafa0d7420f03f7f4f568230e12
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-09-03 21:44:51 -0400

    Test flat/gouraud sprites.

--- tests/gs/simpledraw.cpp
+++ tests/gs/simpledraw.cpp
@@ -1,5 +1,6 @@
 #include <common-ee.h>
 #include <stdio.h>
+#include <assert.h>
 #include "../dma/dmaregs.h"
 #include "../dma/dmasend.h"
 #include "gsregs.h"
@@ -11,13 +12,19 @@
 #define FRAME_HEIGHT  448
 #define FRAME_PSM     GS::PSMCT32
 
+static u16 ftoi4(float value) {
+	assert(value >= 0);
+	value *= 16;
+	return static_cast<u16>(value);
+}
+
 void doTest() {
 	GIF::Tag tag;
-	tag.SetLoops(13);
+	tag.SetLoops(19);
 	tag.SetEop();
 	tag.SetRegDescs(GIF::REG_AD);
 	
-	GIF::PackedPacket packet(64);
+	GIF::PackedPacket packet((19 + 1) * 0x10);
 	packet.WriteTag(tag);
 	
 	//Setup render environment
@@ -30,15 +37,23 @@ void doTest() {
 	//Clear background
 	packet.AD(GS::REG_PRIM,       GS::PRIM(GS::PRIM_SPRITE, 0, 0, 0, 0, 0, 0, 0, 0));
 	packet.AD(GS::REG_RGBAQ,      GS::RGBAQ(0xFF, 0x00, 0xFF, 0xFF, 0));
-	packet.AD(GS::REG_XYZ2,       GS::XYZ2(0, 0, 0));
-	packet.AD(GS::REG_XYZ2,       GS::XYZ2(FRAME_WIDTH * 16, FRAME_HEIGHT * 16, 0));
+	packet.AD(GS::REG_XYZ2,       GS::XYZ2(ftoi4(0), ftoi4(0), 0));
+	packet.AD(GS::REG_XYZ2,       GS::XYZ2(ftoi4(FRAME_WIDTH), ftoi4(FRAME_HEIGHT), 0));
 	
-	//Draw test sprite
+	//Draw test sprite (Flat shaded)
 	packet.AD(GS::REG_PRIM,       GS::PRIM(GS::PRIM_SPRITE, 0, 0, 0, 0, 0, 0, 0, 0));
 	packet.AD(GS::REG_RGBAQ,      GS::RGBAQ(0xCC, 0xDD, 0xEE, 0xAA, 0));
-	packet.AD(GS::REG_XYZ2,       GS::XYZ2(0  * 16, 0  * 16, 0));
-	packet.AD(GS::REG_XYZ2,       GS::XYZ2(32 * 16, 32 * 16, 0));
+	packet.AD(GS::REG_XYZ2,       GS::XYZ2(ftoi4( 0), ftoi4( 0), 0));
+	packet.AD(GS::REG_RGBAQ,      GS::RGBAQ(0x11, 0x22, 0x33, 0xAA, 0));
+	packet.AD(GS::REG_XYZ2,       GS::XYZ2(ftoi4(32), ftoi4(32), 0));
 	
+	//Draw test sprite (Gouraud shaded)
+	packet.AD(GS::REG_PRIM,       GS::PRIM(GS::PRIM_SPRITE, 1, 0, 0, 0, 0, 0, 0, 0));
+	packet.AD(GS::REG_RGBAQ,      GS::RGBAQ(0xCC, 0xDD, 0xEE, 0xAA, 0));
+	packet.AD(GS::REG_XYZ2,       GS::XYZ2(ftoi4(64), ftoi4( 0), 0));
+	packet.AD(GS::REG_RGBAQ,      GS::RGBAQ(0x11, 0x22, 0x33, 0xAA, 0));
+	packet.AD(GS::REG_XYZ2,       GS::XYZ2(ftoi4(96), ftoi4(32), 0));
+
 	DMA::SendSimple(DMA::D2, packet.Raw(), packet.Size());
 	
 	u32 result = getColor(FRAME_ADDRESS, FRAME_WIDTH, FRAME_PSM);
